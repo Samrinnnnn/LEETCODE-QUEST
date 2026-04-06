bf6a4f4097f085562fbc669d83c39fd50a7690b8
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-04-06 16:11:38 +0545

    DATE

--- 02-Filtering-Aggregation-Cabin/filtering_aggregation.sql
+++ 02-Filtering-Aggregation-Cabin/filtering_aggregation.sql
@@ -24,5 +24,14 @@ COALESCE(SUM(amount) FILTER (WHERE state='approved'),0) AS
 FROM transactions
 GROUP BY month,country;
   
+/*4. Write a solution to find the daily active user count for a
+period of 30 days ending 2019-07-27 inclusively. A user was active
+on someday if they made at least one activity on that day. */
+SELECT activity_date AS day,COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date> '2019-07-27'::date-INTERVAL '30 days'
+AND activity_date<='2019-07-27'
+GROUP BY 1;
+
 
 
