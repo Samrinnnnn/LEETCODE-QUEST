ad4bfa9bb659aff8f213deaf18b7c40468edf4db
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-04-10 15:10:15 +0545

    Update grouping_aggregation.sql

--- 03-Grouping-Join-Library/grouping_aggregation.sql
+++ 03-Grouping-Join-Library/grouping_aggregation.sql
@@ -1,1 +1,10 @@
-
+/*1. Write a solution to find the number of times each student
+attended each exam.*/
+SELECT s.student_id,s.student_name,su.subject_name,COUNT(e.student_id)
+AS attended_exams
+FROM students s
+CROSS JOIN subjects su
+LEFT JOIN examinations e ON s.student_id=e.student_id
+AND e.subject_name =su.subject_name
+GROUP BY s.student_id,s.student_name,su.subject_name
+ORDER BY s.student_id,su.subject_name;
